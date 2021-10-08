5c7df6c01a8803afb3b381629e0ce46256109a18
ltoal <47865984+ltoal@users.noreply.github.com>
2021-10-08 14:18:46 -0700

    add checks

--- typecasting/prompt/typecastingprompt.py
+++ typecasting/prompt/typecastingprompt.py
@@ -10,3 +10,18 @@ def exists():
 def compiles():
     """typecastingPrompt.c compiles"""
     check50.c.compile("typecastingPrompt.c", lcs50=True)
+    
+@check50.check(compiles)
+def handles_lowercase_inputs():
+    """typecastingCL.c handles an all-lowercase input"""
+    check50.run("./typecastingPrompt").stdin("computer").stdin("1").stdout("dpnqvufs").exit(0)
+    
+@check50.check(compiles)
+def handles_uppercase_inputs():
+    """typecastingCL.c handles an all-uppercase input"""
+    check50.run("./typecastingPrompt").stdin("COMPUTER").stdin("5").stdout("HTRUZYJW").exit(0)
+
+@check50.check(compiles)
+def handles_mixed_inputs():
+    """typecastingCL.c handles a mix of uppercase/lowercase/symbols"""
+    check50.run("./typecastingPrompt").stdin("Hey!!").stdin("3").stdout("Kh|$$").exit(0)
